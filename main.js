f8c6e6feee15c52b5662d678e59d277390e0d353
Royaldk13 <khotd35@gmail.com>
2024-10-22 22:25:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
 }).listen(8080);
+console.log('Server is running at http://127.0.0.1:8080/');
